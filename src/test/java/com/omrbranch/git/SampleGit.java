d51e50ab7a1eb926e74e26c10b3f01ddbe332deb
JoePraveen05 <joepraveen05@gmail.com>
2026-02-16 21:04:37 +0530

    Hiiiiii.........

---
+++ src/test/java/com/omrbranch/git/SampleGit.java
@@ -0,0 +1,15 @@
+package com.omrbranch.git;
+
+public class SampleGit {
+
+	private void tc1() {
+		System.out.println("test 1");
+
+	}
+
+	private void tc2() {
+		System.out.println("test 2");
+
+	}
+
+}
